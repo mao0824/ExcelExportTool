afe8100d48ec19e21de50e3c855ead20f480f47b
serein <1115772640@qq.com>
2022-04-24 16:11:58 +0800

    ExcelField注解

---
+++ src/main/java/com/zouma/api/annotation/ExcelField.java
@@ -0,0 +1,29 @@
+package com.zouma.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author maogf
+ * @description Excel的功能注解
+ * @date 2022/4/24 15:42
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ExcelField {
+
+    /**
+     * 对应列的索引
+     * @return
+     */
+    int index() default 0;
+
+    /**
+     * 对应列的名称,如id对应的名称为序号
+     * @return 列的名称
+     */
+    String title() default "";
+
+}
